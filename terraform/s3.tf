ca11a15987d4965982fd148542714b465e3215ea
KimTaeyoun <kimty1121@gmail.com>
2026-06-14 23:55:40 +0900

    S3 스트레이지 작성

---
+++ terraform/s3.tf
@@ -0,0 +1,88 @@
+resource "random_string" "s3_unuque_key" {
+  length  = 6
+  upper   = false
+  lower   = true
+  special = false
+}
+
+# -------------------------------------------------
+# S3 static bucket
+# -------------------------------------------------
+resource "aws_s3_bucket" "s3_static_bucket" {
+  bucket = "${var.project}-${var.environment}-static-bucket-${random_string.s3_unuque_key.result}"
+
+  # versioning {
+  #   enabled = false
+  # }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_static_bucket" {
+  bucket = aws_s3_bucket.s3_static_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "s3_static_bucket" {
+  bucket = aws_s3_bucket.s3_static_bucket.id
+  policy = data.aws_iam_policy_document.s3_static_bucket.json
+
+  depends_on = [aws_s3_bucket_public_access_block.s3_static_bucket]
+}
+
+data "aws_iam_policy_document" "s3_static_bucket" {
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.s3_static_bucket.arn}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+# -------------------------------------------------
+# S3 deploy bucket
+# -------------------------------------------------
+resource "aws_s3_bucket" "s3_deploy_bucket" {
+  bucket = "${var.project}-${var.environment}-deploy-bucket-${random_string.s3_unuque_key.result}"
+
+  # versioning {
+  #   enabled = false
+  # }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  policy = data.aws_iam_policy_document.s3_deploy_bucket.json
+
+  depends_on = [aws_s3_bucket_public_access_block.s3_deploy_bucket]
+}
+
+data "aws_iam_policy_document" "s3_deploy_bucket" {
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.s3_deploy_bucket.arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.app_iam_role.arn]
+    }
+  }
+}
